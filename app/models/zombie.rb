dc52914578cee410d5fb915711454d7df16d9f1f
Arush Gupta <arushgupta.me@gmail.com>
2016-06-28 21:29:23 -0700

    has_one relation with dependent actions

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,9 +1,11 @@
 class Zombie < ActiveRecord::Base
 
+	has_one :brain, dependent: :destroy
+
 	# Named Scope
-	scope :rotting, where(rotting: true)
-	scope :fresh, where("age < 20")
-	scope :recent, order("created_at desc").limit(3)
+	#scope :rotting, where(rotting: true)
+	#scope :fresh, where("age < 20")
+	#scope :recent, order("created_at desc").limit(3)
 
 	# Callbacks
 	before_save :make_rotting
